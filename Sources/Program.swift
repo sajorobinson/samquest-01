49573fde9a3f4c92f47c62ae273f1ff4ea623a19
Sam Robinson <sajorobinson@gmail.com>
2025-08-02 11:23:00 -0400

    Added more explicit typing.

--- Sources/Program.swift
+++ Sources/Program.swift
@@ -4,31 +4,45 @@ import Foundation
 struct Main {
     static func main() {
         let player: Creature = Creature(
-            name: "Sam", health: 100, position: (0, 0), behavior: .passive)
+            name: "Sam",
+            health: 100,
+            position: (0, 0),
+            behavior: .passive
+        )
+        
         let goblin: Creature = Creature(
-            name: "Goblin", health: 30, position: (0, 1), behavior: .hostile)
+            name: "Goblin",
+            health: 30,
+            position: (0, 1),
+            behavior: .hostile
+        )
+        
         let villager: Creature = Creature(
-            name: "Villager", health: 100, position: (1, 0), behavior: .passive)
+            name: "Villager", 
+            health: 100, 
+            position: (1, 0), 
+            behavior: .passive
+        )
 
-        let creatures = [player, goblin, villager]
+        let creatures: [Creature] = [player, goblin, villager]
 
-        let locations = [
+        let locations: [Location] = [
             Location(position: (0, 0), name: "Village Square"),
             Location(position: (0, 1), name: "Goblin Cave"),
         ]
 
-        let initialState = GameState(
+        let initialState: GameState = GameState(
             player: player,
             creatures: creatures,
             locations: locations,
             isGameOver: false,
-            mapBounds: (minX: -5, maxX: 5, minY: -5, maxY: 5)
+            mapBounds: MapBounds(minX: -5, maxX: 5, minY: -5, maxY: 5)
         )
 
-        let engine = GameEngine(initialState: initialState)
+        let engine: GameEngine = GameEngine(initialState: initialState)
 
         while !engine.state.isGameOver {
-            let actions = engine.availableActions()
+            let actions: [Action] = engine.availableActions()
             print("=========================")
             print("||      SamQuest01     ||")
             print("=========================")
@@ -38,12 +52,13 @@ struct Main {
             }
             print("INPUT: ", terminator: "")
 
-            if let input = readLine(), let choice = Int(input), choice > 0, choice <= actions.count
+            if let input: String = readLine(), let choice: Int = Int(input), choice > 0,
+                choice <= actions.count
             {
-                let result = engine.perform(actions[choice - 1])
+                let result: String = engine.perform(actions[choice - 1])
                 PU.d(m: result)
-                let aiEvents = engine.updateCreatures()
-                for event in aiEvents {
+                let aiEvents: [String] = engine.updateCreatures()
+                for event: String in aiEvents {
                     PU.d(m: event)
                 }
             } else {
