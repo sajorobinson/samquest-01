f1c5f0196217b0d7168c3104e0c282544b4aba43
Sam Robinson <sajorobinson@gmail.com>
2025-08-06 23:05:57 -0400

    Built out new location design.

--- Sources/Models/Location.swift
+++ Sources/Models/Location.swift
@@ -1,3 +1,10 @@
 struct Location {
     let name: String
+    let description: String
+    var exits: [Direction: Location] = [:]
+
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+    }
 }
