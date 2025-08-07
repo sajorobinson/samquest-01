f6202fa41e559e7594e15d73d27880491ac89388
Sam Robinson <sajorobinson@gmail.com>
2025-08-06 23:32:22 -0400

    Thinking about how to do scenes.

--- Sources/Models/GameEngine.swift
+++ Sources/Models/GameEngine.swift
@@ -10,19 +10,36 @@ class GameEngine {
         actions.append(.examine)
         actions.append(.check)
         actions.append(.exit)
-
         return actions
     }
 
     func perform(_ action: Action) -> String {
         switch action {
         case .examine:
+            // Location examination scene.
+            /*
+                Thinking conceptually for all scenes, we have some things to consider:
+            
+                1. Who started the scene? The player or an NPC (random encounter)?
+                2. Who is involved in the scene? The player, allies of the player, an enemy, etc?
+                3. What can you do in the scene? This is dependent on the Scene.sceneType.
+                    And honestly each scene will probably need to just be a new class & inherit the Scene class.
+            
+                This information is like the "scene resources", we're gathering everything up that the
+                scene needs to play out.
+            
+                Then, the engine can run the scene, giving each creature a turn and an opportunity
+                to make a decision.
+            */
             return "A location description."
         case .check:
+            // Player stats & health scene
             return state.player.announceHealth()
         case .talk:
+            // Conversation scene
             return "You talk to the creature. They look at you curiously."
         case .attack:
+            // Combat scene
             return "You make a sneering face at the creature."
         case .exit:
             state.isGameOver = true
