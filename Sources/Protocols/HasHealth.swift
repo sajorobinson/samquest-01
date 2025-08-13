b91aa2dd699da7291918307c4aea425169bf23eb
Sam Robinson <sajorobinson@gmail.com>
2025-08-12 23:50:39 -0400

    Removed whitespace.

--- Sources/Protocols/HasHealth.swift
+++ Sources/Protocols/HasHealth.swift
@@ -8,7 +8,6 @@ extension HasHealth {
     func getHealth() -> String {
         return "\(self.health)"
     }
-
     mutating func changeHealth(by amount: Int) -> Int {
         self.health += amount
         return self.health
