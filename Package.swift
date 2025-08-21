a991a80925bef48e923d522b463fb3af39e3f5ea
Sam Robinson <sajorobinson@gmail.com>
2025-08-21 16:54:55 -0400

    Removed this dependency for now as I focus on core logic first.

--- Package.swift
+++ Package.swift
@@ -5,17 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "SamQuest01",
-    dependencies: [
-        .package(url: "https://github.com/jpsim/Yams.git", from: "6.0.0")
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "SamQuest01",
-            dependencies: [
-                "Yams"
-            ]
+            dependencies: []
         )
     ]
 )
