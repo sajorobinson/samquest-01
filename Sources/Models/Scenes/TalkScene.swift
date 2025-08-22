b15261008e73b5dac1335b9e632170c4b1b983aa
Sam Robinson <sajorobinson@gmail.com>
2025-08-22 19:20:35 -0400

    Hooked up basic talking scene.

--- Sources/Models/Scenes/TalkScene.swift
+++ Sources/Models/Scenes/TalkScene.swift
@@ -1,6 +1,24 @@
 struct TalkScene {
     var isSceneOver: Bool = false
     mutating func run(with state: inout GameState) -> String {
-        return "Hello, world!"
+        while !self.isSceneOver {
+            print("Who do you want to talk to?")
+            for (i, entity) in state.entities.enumerated() {
+                print("[\(i + 1)] \(entity.name)")
+            }
+            print("INPUT: ", terminator: "")
+            guard
+                let input = readLine(),
+                let choice = Int(input),
+                (1...state.entities.count).contains(choice)
+            else {
+                print("Invalid input. Please enter a number from the list.")
+                continue
+            }
+            let target = state.entities[choice - 1]
+            let result = target.speak()
+            return result
+        }
+        return "Default speech."
     }
 }
