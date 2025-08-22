84ae62f569d5967aa8701c98a20847f0126c4708
Sam Robinson <sajorobinson@gmail.com>
2025-08-22 19:40:07 -0400

    Added very basic attack logic.

--- Sources/Models/Scenes/AttackScene.swift
+++ Sources/Models/Scenes/AttackScene.swift
@@ -1,6 +1,6 @@
 struct AttackScene {
     var isSceneOver: Bool = false
     mutating func run(with state: inout GameState) -> String {
-        return "You hurt yourself in confusion!"
+        return "You hurt yourself in confusion! \(state.playerCharacter.changeHealth(by: -10))."
     }
 }
