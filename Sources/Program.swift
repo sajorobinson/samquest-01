1cef48b8cad5c6715e4e5c4237c4b3f355fc2829
Sam Robinson <sajorobinson@gmail.com>
2025-08-22 01:01:05 -0400

    Whitespace.

--- Sources/Program.swift
+++ Sources/Program.swift
@@ -5,17 +5,14 @@ struct Main {
     static func main() {
         let initialState = GameManager.createGameState()
         let engine = GameEngine(initialState: initialState)
-
         while !engine.isGameOver {
             let actions = engine.listAvailableActions()
-
             print("SamQuest01")
             print("Choose an action:")
             for (i, action) in actions.enumerated() {
                 print("[\(i + 1)] \(action.description)")
             }
             print("INPUT: ", terminator: "")
-
             guard
                 let input = readLine(),
                 let choice = Int(input),
@@ -24,7 +21,6 @@ struct Main {
                 print("Invalid input. Please enter a number from the list.")
                 continue
             }
-
             let result = engine.perform(actions[choice - 1])
             print(result)
         }
