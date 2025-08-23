901a9b162ab56820e3bbc9fc1e84add5c6286795
Sam Robinson <sajorobinson@gmail.com>
2025-08-23 19:48:25 -0400

    Simplified movement.

--- Sources/Models/Scenes/Move.swift
+++ Sources/Models/Scenes/Move.swift
@@ -1,40 +1,31 @@
 struct Move: Scene {
     var isSceneOver: Bool = false
     mutating func run(with state: inout GameState) -> String {
-        while !self.isSceneOver {
-            print("Which way do you want to go?")
-            let directions = Direction.listDirections()
-            for (i, direction) in directions.enumerated() {
-                print("[\(i + 1)] \(direction)")
-            }
-            print("INPUT: ", terminator: "")
-            guard
-                let input = readLine(),
-                let choice = Int(input),
-                (1...directions.count).contains(choice)
-            else {
-                print("Invalid input. Please enter a number from the list.")
-                continue
-            }
-            let direction = directions[choice - 1]
+        print("Which direction do you want to move?")
+        print("w = north")
+        print("a = west")
+        print("s = south")
+        print("d = east")
+        print("x = cancel")
 
-            if direction == .north {
-                state.playerCharacter.posY += 1
-                return "You move north."
-            } else if direction == .south {
-                state.playerCharacter.posY += -1
-                return "You move south."
-            } else if direction == .east {
-                state.playerCharacter.posX += 1
-                return "You move east."
-            } else if direction == .west {
-                state.playerCharacter.posX += -1
-                return "You move west."
-            } else {
-                state.playerCharacter.posX += 0
-                state.playerCharacter.posY += 0
-                return "You stand still."
-            }
+        let choice = readLine() ?? "x"
+
+        if choice.capitalized == "W" {
+            state.playerCharacter.posY += 1
+            return state.playerCharacter.getPosition()
+        } else if choice.capitalized == "A" {
+            state.playerCharacter.posX += -1
+            return state.playerCharacter.getPosition()
+        } else if choice.capitalized == "S" {
+            state.playerCharacter.posY += -1
+            return state.playerCharacter.getPosition()
+        } else if choice.capitalized == "D" {
+            state.playerCharacter.posX += 1
+            return state.playerCharacter.getPosition()
+        } else if choice.capitalized == "X" {
+            return "Movement canceled."
+        } else {
+            print("Invalid input. Movement canceled.")
         }
         return "Default movement."
     }
