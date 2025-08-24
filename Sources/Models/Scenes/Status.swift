138f9805c296b2303f4a07f3db75c8d99d7473c8
Sam Robinson <sajorobinson@gmail.com>
2025-08-23 23:27:20 -0400

    Improved logic for building player status.

--- Sources/Models/Scenes/Status.swift
+++ Sources/Models/Scenes/Status.swift
@@ -1,23 +1,20 @@
 struct Status: Scene {
     var isSceneOver: Bool = false
     mutating func run(with state: inout GameState) -> String {
+        var payloadContainer: [String] = []
         let player = state.playerCharacter
+
+        payloadContainer.append("Your health: \(state.playerCharacter.getHealth())")
+        payloadContainer.append("x: \(player.posX), y: \(player.posY)")
 
         if let location = state.locations.first(where: {
             $0.x == player.posX && $0.y == player.posY
         }) {
-            return """
-                Your health: \(state.playerCharacter.getHealth())
-                Your position: x: \(player.posX), y: \(player.posY)
-                Location: \(location.name)
-                Description: \(location.description)
-                """
+            payloadContainer.append("Location: \(location.name)")
+            payloadContainer.append("Description: \(location.description)")
+        } else {
+            payloadContainer.append("There's nothing here.")
         }
-
-        return """
-            Your health: \(state.playerCharacter.getHealth())
-            x: \(player.posX), y: \(player.posY)
-            "There's nothing here."
-            """
+        return payloadContainer.joined(separator: "\n")
     }
 }
