de85221866b5b6e3ac40fdd1f8897a2a1cf54d79
Sam Robinson <sajorobinson@gmail.com>
2025-08-24 00:02:30 -0400

    Switched debug options to case statement.

--- Sources/Models/Scenes/Debug.swift
+++ Sources/Models/Scenes/Debug.swift
@@ -1,25 +1,16 @@
 struct Debug: Scene {
     var isSceneOver: Bool = false
-    var debugUserInterface: String {
-        """
-        Select a debug command
-        | pe = print all entities | pl = print all locations |
-        | x = cancel  |
-        """
-    }
     mutating func run(with state: inout GameState) -> String {
-        print(debugUserInterface)
         let choice = Utilities.InputOutput.readInput()
-        if choice.lowercased() == "pe" {
+        switch choice.lowercased() {
+        case "pe":
             let results = Utilities.Debug.listEntities(with: state).joined(separator: "\n")
             return results
-        } else if choice.lowercased() == "pl" {
+        case "pl":
             let results = Utilities.Debug.listLocations(with: state).joined(separator: "\n")
             return results
-        } else if choice.lowercased() == "x" {
-            return "Debug canceled."
-        } else {
-            return "Invalid input. Debug canceled."
+        default:
+            return "Invalid input."
         }
     }
 }
