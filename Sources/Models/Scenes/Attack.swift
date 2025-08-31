cff9eaaa148c51a52f1a4de865d3a200fb7962d9
Sam Robinson <sajorobinson@gmail.com>
2025-08-31 00:10:49 -0400

    Added the very first combat logic

--- Sources/Models/Scenes/Attack.swift
+++ Sources/Models/Scenes/Attack.swift
@@ -1,6 +1,27 @@
 struct Attack: Scene {
     var isSceneOver: Bool = false
     mutating func run(with state: inout GameState) -> String {
-        return "You hurt yourself in confusion! \(state.playerCharacter.changeHealth(by: -10))."
+        while !self.isSceneOver {
+            print("Choose something to attack.")
+            let entities = Utilities.Entities.listEntitiesAtPosition(
+                with: state,
+                x: state.playerCharacter.posX,
+                y: state.playerCharacter.posY
+            )
+            if entities.count == 0 {
+                return "There's nothing to attack here."
+            } else {
+                Utilities.Entities.printEntities(entities)
+                let choice = Utilities.Entities.readChosenEntity(entities)
+                if choice == nil {
+                    continue
+                } else {
+                    let totalDamage = Int.random(in: 1..<5)
+                    choice!.changeHealth(by: totalDamage)
+                    return "You attack \(choice!.name), dealing \(totalDamage) damage."
+                }
+            }
+        }
+        return "Default attack."
     }
 }
